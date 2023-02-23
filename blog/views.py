20657c0b787dc2169f261bdbbaa821fca2da6402
ferduarte <ferduarte@live.com.ar>
2023-02-23 19:29:25 -0300

    Updated files. Added a Post class in models.py. Querys from python shlell. Edited views.py, deleted dummy data. Edited date format in home.html.

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author':'Fernando DD',
-        'title':'My Blog post 1',
-        'content':'First post content',
-        'date_posted':'February 15, 2023'
-    },
-    {
-        'author':'Shiva DD',
-        'title':'My Blog post 2',
-        'content':'Second post content',
-        'date_posted':'February 16, 2023'
-    }
-]
+from .models import Post
 
 
 def home(request):
 
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     
     return render(request, 'blog/home.html', context)
